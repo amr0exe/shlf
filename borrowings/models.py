9001e7b46b834872aef38f122300c3dd8b68c63e
Amrit Thapa <zyklen908@gmail.com>
2026-07-19 18:37:14 +0545

    feat: implement borrowings and members model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from members.models import Member
+from books.models import BookCopy
 
 # Create your models here.
+class Borrowing(models.Model):
+    class BorrowStatus(models.TextChoices):
+        BORROWED = 'borrowed', 'Borrowed'
+        RETURNED = 'returned', 'Returned'
+        OVERDUE = 'overdue', "Overdue"
+        LOST = 'lost', 'Lost'
+
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.RESTRICT,
+        related_name="borrowings"
+    )
+    copy = models.ForeignKey(
+        BookCopy,
+        on_delete=models.RESTRICT,
+        related_name="borrowings"
+    )
+
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    due_at = models.DateTimeField()
+    returned_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=15,
+        choices=BorrowStatus.choices,
+        default=BorrowStatus.BORROWED
+    )
+
+    def __str__(self):
+        return f"Borrow {self.pk}: {self.member.name} -> {self.copy.book.title}"
+
+
+class Fine(models.Model):
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="fines"
+    )
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Fine {self.amount}"
--- members/models.py
+++ members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
